64631f5910d5e2ef54ac8886ff939f971db4776f
Jim Chen <knh.jabbany@gmail.com>
2014-12-10 16:31:55 -0500

    Add support for sharess

--- routes/media.js
+++ routes/media.js
@@ -1,12 +1,17 @@
 var userlib = require("../lib/users.js");
+var sharelib= require("../lib/shares.js");
 exports.photos = function(req, res){
 	if(!req.user.login){
 		res.redirect(302, '/login');
 		return;
 	}
-	res.render('photo', {
-		login:req.user.login,
-		user:req.user.user
+	var shareinst = new sharelib(req.db);
+	shareinst.getShareable(req.param("id"), function(data){
+		console.log(data);
+		res.render('photo', {
+			login:req.user.login,
+			user:req.user.user
+		});
 	});
 };
 
