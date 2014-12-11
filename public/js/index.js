d04b644c71c209bf5bb7bf68c4ff0e8928429ecb
Jim Chen <knh.jabbany@gmail.com>
2014-12-11 01:20:36 -0500

    Add full support for friends recommendations

--- public/js/index.js
+++ public/js/index.js
@@ -25,4 +25,33 @@ window.addEventListener('load', function(){
 			}
 		}
 	});
+	
+	// Friend recommendations
+	$.ajax({
+		type: "GET",
+		url: "/api/user/recommend/friends",
+		dataType: "json",
+		success: function(data){
+			if(typeof data === "string"){
+				try{
+					data = JSON.parse(data);
+				}catch(e){}
+			}
+			$("#recommend-friends").empty();
+			if(data.code === 200){
+				for(var i = 0; i < data.recommend.length; i++){
+					$("#recommend-friends").append(""
+						+ "<a class='list-group-item' href='/profile/" + data.recommend[i].username + "'>"
+						+ "<img src='" + data.recommend[i].avatar + "?s=32' class='pull-left' style='width:32px; height:32px;'>"
+						+ "<div style='margin-left:40px; margin-bottom:12px;'>" + data.recommend[i].fullname + "</div>"
+						+ "</a>");
+				}
+				if(data.recommend.length === 0){
+					$("#recommend-friends").append("<div class='list-group-item'>No Records</div>");
+				};
+			}else{
+				$("#recommend-friends").append("<div class='list-group-item'>No Records</div>");
+			}
+		}
+	});
 });
