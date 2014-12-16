6fe434d26e8c246f22483486e5539471959a0289
leonardloo <leonard.loo.ks@gmail.com>
2014-12-15 21:12:29 -0500

    invite on profile done

--- public/js/profile.js
+++ public/js/profile.js
@@ -47,65 +47,70 @@ window.addEventListener('load', function(){
 		});
 	});
 
+
+
+	// Upon page load, get invitables
+	$.ajax({
+		type: "GET",
+		url: "/api/user/" + $("#uid").text() + "/invitables",
+	 	success: function(data){
+	 		var trips = data.invitables;
+	 		if (trips && trips.length > 0) {
+	 			$("#invitables").html('');
+	 			$('#invite-trip').show();
+	 			console.log(trips);
+	 			for (var i = 0; i < trips.length; i++) {
+	 				var trip = $('<div class="row" style="padding-bottom:4px;" id="tripid' + trips[i].trip_id + '"></div>');
+	 				var buttonCol = $('<div class="col-md-12">' + trips[i].description + '</div>');
+	 				var button = $('<button class="btn btn-default pull-right">Invite</button>');
+	 				buttonCol.append(button);
+	 				trip.append(buttonCol);
+	 				$(button).click((function(){
+	 					var tid = trips[i].trip_id;
+	 					return function(){
+		 					$.ajax({
+								type: "POST",
+								url: "/api/user/invite",
+								data: {
+									"tid": tid,
+									"target": $("#uid").text()
+								},
+								dataType: "json",
+							 	success: function(data){
+							 		if(typeof data === "string"){
+							 			try{
+							 				data = JSON.parse(data);
+							 			}catch(e){}
+							 		}
+							 		if(data.code === 200){
+							 			console.log('Successfully invited');
+							 			$('#tripid' + tid).fadeOut();
+							 		} else  {
+							 			alert('An error occurred. Try inviting again later.');
+							 		}
+								}
+							});
+	 					}
+	 				})());
+	 				$("#invitables").append(trip);
+	 			}
+	 		} else {
+	 			$('#invite-trip').hide();
+	 		}
+		}
+	});
+
 	var inviteIsOpen = false;
 
 	$("#invite-trip").click(function(){
 		inviteIsOpen = !inviteIsOpen;
-		if (!inviteIsOpen) {
-			$("#invite-trip").text('Invite to Trip(s) ↓');
-			$("#invitables").html('');
-			$("#invitables").hide();
+		if (inviteIsOpen) {
+			$("#invite-trip").text('Invite to Trip(s) ↑');
+			$("#invitables").show();
 			return;
 		}
-		$.ajax({
-			type: "GET",
-			url: "/api/user/" + $("#uid").text() + "/invitables",
-		 	success: function(data){
- 				$("#invite-trip").text('Invite to Trip(s) ↑');
-		 		var trips = data.invitables;
-		 		if (trips && trips.length > 0) {
-		 			$("#invitables").html('');
-		 			console.log(trips);
-		 			for (var i = 0; i < trips.length; i++) {
-		 				var trip = $('<div class="row"></div>');
-		 				var buttonCol = $('<div class="col-md-12">' + trips[i].description + '</div>');
-		 				var button = $('<button class="btn btn-default pull-right">' + trips[i].description+ '</button>');
-		 				buttonCol.append(button);
-		 				trip.append(buttonCol);
-		 				$(button).click((function(){
-		 					var tid = trips[i].trip_id;
-		 					return function(){
-			 					$.ajax({
-									type: "POST",
-									url: "/api/user/invite",
-									data: {
-										"tid": tid,
-										"target": $("#uid").text()
-									},
-									dataType: "json",
-								 	success: function(data){
-								 		if(typeof data === "string"){
-								 			try{
-								 				data = JSON.parse(data);
-								 			}catch(e){}
-								 		}
-								 		if(data.code === 200){
-								 			console.log('Successfully invited');
-								 		} else  {
-								 			alert(data.msg);
-								 		}
-									}
-								});
-		 					}
-		 				})());
-		 				$("#invitables").append(trip);
-		 			}
-		 		} else {
-			 		var error = 'No trip to invite to.';
-			 		$("#invitables").append(error);
-			 		$("#invitables").show();
-		 		}
-			}
-		});
+		$("#invite-trip").text('Invite to Trip(s) ↓');
+		$("#invitables").hide();
 	});
+
 });
